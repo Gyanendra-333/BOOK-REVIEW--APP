dd5663265afff2e81861b80d437356465dc2f180
Gyanendra prakash <pgyanendra333@gmail.com>
2022-12-21 04:25:23 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Home from './features/book/components/Home/Home';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Book from './features/book/components/Book/Book';
+
+function App() {
+  return (
+   <div></div>
+     
+  );
+}
+
+export default App;
